9cb221975cd4a6ae7ea33374b42b9e2be4dcd1c7
Vitor.Ochoa <vitor.ochoa@sequor.com.br>
2021-10-26 21:22:42 -0300

    FrontEnd - Create Responsive NavBar

--- Front/ProEvents-App/src/app/app.module.ts
+++ Front/ProEvents-App/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { EventsComponent } from './lectureEvents/lectureEvents.component';
 import { PanelistsComponent } from './panelists/panelists.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    PanelistsComponent
+    PanelistsComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
